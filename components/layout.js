b99a1b33a5e5b13412cc28861b0c385b901b03c8
setareEzabadi <127828381+setareEzabadi@users.noreply.github.com>
2025-04-06 23:24:50 +0330

    Create layout.js

---
+++ components/layout.js
@@ -0,0 +1,13 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+
+export default function Layout({ children }) {
+
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+}
