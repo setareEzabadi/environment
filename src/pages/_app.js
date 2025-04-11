c71b5b48294e465af396ef246bfe9fc41f963792
setareEzabadi <127828381+setareEzabadi@users.noreply.github.com>
2025-04-11 23:14:23 +0330

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,7 @@
 import "@/styles/globals.css";
 import Layout from "../../components/layout";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export default function App({ Component, pageProps }) {
   return (
