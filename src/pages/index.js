5a04ec75f0fdc35e95e12fd1c03c8a13d7d4d1eb
setareEzabadi <127828381+setareEzabadi@users.noreply.github.com>
2025-04-11 23:14:30 +0330

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import styles from "@/styles/Home.module.css";
+import LandingPage from "../../components/LandingPage/LandingPage";
 
 export default function Home() {
   return (
     <>
+      <LandingPage />
     </>
   );
 }
