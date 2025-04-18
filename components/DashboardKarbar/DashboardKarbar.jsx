46f86ae27573bbb5ed928df3e6468e6c62147f39
setareEzabadi <127828381+setareEzabadi@users.noreply.github.com>
2025-04-18 09:37:48 +0330

    Create DashboardKarbar.jsx

---
+++ components/DashboardKarbar/DashboardKarbar.jsx
@@ -0,0 +1,11 @@
+import styles from "./DashboardKarbar.module.css";
+
+const DashboardKarbar = () => {
+    return (
+        <div className={styles.DashboardKarbar}>
+
+        </div>
+    );
+};
+
+export default DashboardKarbar;
