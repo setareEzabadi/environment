d632baa43826fd8552a15265db7b37528d2e5c12
setareEzabadi <127828381+setareEzabadi@users.noreply.github.com>
2025-04-19 13:41:42 +0330

    Create Reports.jsx

---
+++ components/DashboardKarbar/Reports.jsx
@@ -0,0 +1,12 @@
+import styles from './DashboardKarbar.module.css';
+
+const Reports = () => {
+    return (
+        <div className={styles.Reports}>
+            <h3>گزارش‌های ثبت‌شده</h3>
+            <p>لیست گزارش‌های شما اینجا نمایش داده می‌شود.</p>
+        </div>
+    );
+};
+
+export default Reports;
