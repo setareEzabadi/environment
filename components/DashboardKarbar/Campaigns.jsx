761284211376325232dfc830d1f3eba4bf0ed7d0
setareEzabadi <127828381+setareEzabadi@users.noreply.github.com>
2025-04-19 13:41:31 +0330

    Create Campaigns.jsx

---
+++ components/DashboardKarbar/Campaigns.jsx
@@ -0,0 +1,12 @@
+import styles from './DashboardKarbar.module.css';
+
+const Campaigns = () => {
+    return (
+        <div className={styles.Campaigns}>
+            <h3>کمپین‌های شرکت‌شده</h3>
+            <p>لیست کمپین‌هایی که در آن‌ها شرکت کرده‌اید.</p>
+        </div>
+    );
+};
+
+export default Campaigns;
