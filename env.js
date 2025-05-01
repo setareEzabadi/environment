61f1af981922bb18361f9b20af7863cd870c27a9
setareEzabadi <127828381+setareEzabadi@users.noreply.github.com>
2025-05-01 13:56:31 +0330

    Update env.js

--- env.js
+++ env.js
@@ -1,10 +1,5 @@
 const env = {
   baseUrl: "http://127.0.0.1:8000/",
-  division: 1000,
-  WEBSOCKET_HOST: "localhost:8765",
-  WEBSOCKET_HTTP: "ws",
-  WEBSOCKET_KEY: "token1",
-  basePath: "",
 };
 
 export default env;
