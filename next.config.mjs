d29baf26f84399dc957d674530a574343a0d52f5
setareEzabadi <127828381+setareEzabadi@users.noreply.github.com>
2025-05-31 12:47:11 +0330

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages: ["@mui/material", "@mui/x-date-pickers"],
+  experimental: {
+    esmExternals: "loose",
+  },
 };
 
 export default nextConfig;
